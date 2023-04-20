d6ca6533e190b37782596abdbe66e08e8fdea3b0
fxzer <wy3010099292@163.com>
2023-04-20 15:31:52 +0800

    fix: vercel deploy baseurl

--- docs/.vitepress/configs/shared.ts
+++ docs/.vitepress/configs/shared.ts
@@ -5,10 +5,8 @@ export const META_IMAGE = 'https://pinia.vuejs.org/social.png'
 
 export const sharedConfig = defineConfig({
   //根据环境变量决定打包路径
-  // base:'/',
+  base:'/',
   // srcDir:'./src', //相对于docs
-  // base: process.env.NODE_ENV === 'production' ? '/tsdoc-vitepress/' : '/',
-  base: '/tsdoc-vitepress/',//配置打包获取静态资源路径
   outDir: '../dist',
   cleanUrls: true,  //去掉url中的.html后缀
   title: "TSDoc",//网站标题

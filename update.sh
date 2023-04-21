7bd109d44f7943053d9266e3f4200fa6868c6869
fxzer <wy3010099292@163.com>
2023-04-21 14:33:32 +0800

    delete: workflow

--- update.sh
+++ update.sh
@@ -17,6 +17,4 @@ git commit -m 'feat: update dist'
 git push -f git@github.com:fxzer/tsdoc-vitepress.git master:gh-pages
 git push -f git@gitee.com:fxzer/tsdoc-vitepress.git master:gh-pages
 
-vercel --prod
-
 cd -
